6c71bfd21d1f8f7373117537a10c48ed1997867f
Hicham <121503241+HichamChlghami@users.noreply.github.com>
2024-08-12 19:32:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -268,7 +268,7 @@ app.get('/' , (req , res)=>{
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
